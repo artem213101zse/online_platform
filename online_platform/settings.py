42835116c5dd1f5f640027f906b5a5e4c7bf0146
Artem Kononov <artem213101zse@gmail.com>
2021-09-23 21:31:12 +0300

    Deploy

--- online_platform/settings.py
+++ online_platform/settings.py
@@ -120,11 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_URL = '/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
