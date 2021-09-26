4a9f1f64d396313d4c43d5031d7631a20672160d
Artem Kononov <artem213101zse@gmail.com>
2021-09-26 18:13:43 +0300

    Disable debug mode

--- online_platform/settings.py
+++ online_platform/settings.py
@@ -28,7 +28,7 @@ if os.environ.get('DJANGO_DEBUG'):
     # When not specified, ALLOW_HOSTS defaults to:
     # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 else:
-    DEBUG = True
+    DEBUG = False
     print("Debug is disables.")
 
 
